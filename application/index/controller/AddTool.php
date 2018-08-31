e61d04a0f885d8ac844146ed4b5d07dee3a034f3
ss7424Refar <812647742@qq.com>
2018-08-31 17:23:21 +0800

    create select2 by source_20180831

--- application/index/controller/AddTool.php
+++ application/index/controller/AddTool.php
@@ -24,12 +24,46 @@ class AddTool extends Controller
         $res = $atsTaskPanel->where('tool_name', $selection)->select();
         $resLength = count($res);
 
+        $threshold = 1; // 阀值
+
         $htmlCreated = "";
-        for ($i=0; $i<count($resLength); $i++){
+        // create panelHead (single data)
+        $timeStamp = time();
+        $panel = $atsTaskPanel->where('tool_name', $selection)->find();
+        $htmlCreated = $htmlCreated. CreateHtmlElement::panelInit($panel['tool_name'], $panel['panel_class'], $timeStamp);
+
+        for ($i=0; $i<$resLength; $i++){
+            if(1 == $threshold) {
+                $htmlCreated = $htmlCreated.
+                                '        <div class="form-group">';
+            }
+
+            else if(MAX_LINE_LENGTH == $threshold){
+                $htmlCreated = $htmlCreated.
+                                '        </div>';
+                $threshold++;
+            }
+//            else {
+//                $htmlCreated = $htmlCreated.
+//                                '        <div class="form-group">';
+//                $threshold = 1;
+//            }
+
+            // get Type
             if(SELECT2 == $res[$i]['html_type']){
-                $htmlCreated = $htmlCreated . CreateHtmlElement::select2Init($res[$i]);
+                $htmlCreated = $htmlCreated . CreateHtmlElement::select2Init($res[$i], $threshold);
+                $threshold ++;
+
 
             }
+
+//            else if(SELECT == $res[$i]['html_type']){
+//                $htmlCreated = $htmlCreated . CreateHtmlElement::selectInit($res[$i], $threshold);
+//            } else if(RADIO == $res[$i]['html_type']){
+//
+////                $htmlCreated = $htmlCreated . CreateHtmlElement::select2Init($res[$i], $threshold);
+//            }
+
 
         }
 
@@ -38,6 +72,33 @@ class AddTool extends Controller
 
     }
 
+    public function getTestImage(){
+        $query = $this->request->param('q');;
+        $handler = opendir(ATS_IMAGES_PATH);
+
+        $i = 1;
+        $jsonResult = array();
+
+        while (($filename = readdir($handler)) !== false) {
+            //略过linux目录的名字为'.'和‘..'的文件
+            if ($filename != "." && $filename != "..") {
+                if (empty(trim($query))) {
+                    $tmpArray = array('id' => $filename, 'text' => $filename);
+                    $i = $i + 1;
+                    array_push($jsonResult, $tmpArray);
+                } else {
+                    if (stristr($filename, $query) !== false) {
+                        $tmpArray = array('id' => $filename, 'text' => $filename);
+                        $i = $i + 1;
+                        array_push($jsonResult, $tmpArray);
+                    }
+                }
+            }
+        }
+
+        return json_encode($jsonResult);
+
+    }
 
 
 }
