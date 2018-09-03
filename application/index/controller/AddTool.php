f952e269507d1d788771404801935e9c10126847
ss7424Refar <812647742@qq.com>
2018-09-03 17:21:54 +0800

    addTool_by_dynamic_create_20180903

--- application/index/controller/AddTool.php
+++ application/index/controller/AddTool.php
@@ -18,56 +18,75 @@ class AddTool extends Controller
     public function createHtmlElement(){
 
         $selection = $this->request->param('selection');// 需要继承控制器
+        $collapseId = $this->request->param('collapseId');
 
         $atsTaskPanel = new AtsTaskPanel();
 
         $res = $atsTaskPanel->where('tool_name', $selection)->select();
         $resLength = count($res);
 
-        $threshold = 1; // 阀值
+        $threshold = 0; // 阀值
 
         $htmlCreated = "";
         // create panelHead (single data)
-        $timeStamp = time();
         $panel = $atsTaskPanel->where('tool_name', $selection)->find();
-        $htmlCreated = $htmlCreated. CreateHtmlElement::panelInit($panel['tool_name'], $panel['panel_class'], $timeStamp);
+        $htmlCreated = $htmlCreated. CreateHtmlElement::panelInit($panel['tool_name'], $panel['panel_class'], $collapseId);
 
-        for ($i=0; $i<$resLength; $i++){
-            if(1 == $threshold) {
-                $htmlCreated = $htmlCreated.
-                                '        <div class="form-group">';
+        $trLength = $resLength % MAX_LINE_LENGTH == 0 ? ($resLength / MAX_LINE_LENGTH) : intval($resLength / MAX_LINE_LENGTH) + 1;
+
+//        $tmpArr = array_slice($res, 2, MAX_LINE_LENGTH);
+//        var_dump($tmpArr[0]['html_type']) ;
+
+        $count = 0;
+        for ($i = 0; $i < $trLength; $i++){
+            $htmlCreated = $htmlCreated. ' <div class="form-group">';
+            $tmpArr = array_slice($res, $i*MAX_LINE_LENGTH, MAX_LINE_LENGTH);
+//            var_dump(count($tmpArr));
+            for($j = 0; $j < count($tmpArr); $j++){
+
+                // get Type
+                if(SELECT2 == $tmpArr[$j]['html_type']){
+                    $htmlCreated = $htmlCreated . CreateHtmlElement::select2Init($tmpArr[$j]);
+                    $count++;
+                }
+                else if(SELECT == $tmpArr[$j]['html_type']){
+                    $htmlCreated = $htmlCreated . CreateHtmlElement::selectInit($tmpArr[$j]);
+                    $count++;
+                }
+                else if(RADIO == $tmpArr[$j]['html_type']){
+                    $htmlCreated = $htmlCreated . CreateHtmlElement::radioInit($tmpArr[$j], $collapseId);
+                    $count++;
+                }
+                if (MAX_LINE_LENGTH == $count){
+                    $count = 0;
+                    break;
+                }
             }
 
-            else if(MAX_LINE_LENGTH == $threshold){
-                $htmlCreated = $htmlCreated.
-                                '        </div>';
-                $threshold++;
-            }
-//            else {
-//                $htmlCreated = $htmlCreated.
-//                                '        <div class="form-group">';
-//                $threshold = 1;
-//            }
-
-            // get Type
-            if(SELECT2 == $res[$i]['html_type']){
-                $htmlCreated = $htmlCreated . CreateHtmlElement::select2Init($res[$i], $threshold);
-                $threshold ++;
-
-
-            }
-
-//            else if(SELECT == $res[$i]['html_type']){
-//                $htmlCreated = $htmlCreated . CreateHtmlElement::selectInit($res[$i], $threshold);
-//            } else if(RADIO == $res[$i]['html_type']){
-//
-////                $htmlCreated = $htmlCreated . CreateHtmlElement::select2Init($res[$i], $threshold);
-//            }
-
-
+            $htmlCreated = $htmlCreated. ' </div>';
         }
 
-
+//        for ($i=0; $i<$resLength; $i++){
+//            if(0 == $threshold) {
+//                $htmlCreated = $htmlCreated.
+//                                '        <div class="form-group">';
+//            }
+//            if(MAX_LINE_LENGTH < $threshold){
+//                $htmlCreated = $htmlCreated.
+//                    '        <div class="form-group">';
+//                $threshold = 0;
+//            }
+//
+//
+//            if(MAX_LINE_LENGTH == $threshold){
+//                $htmlCreated = $htmlCreated.
+//                    '        </div>';
+//                $threshold ++;
+//            }
+//
+//        }
+        // panel footer
+        $htmlCreated = CreateHtmlElement::panelFooter($htmlCreated);
         return $htmlCreated;
 
     }
