acc00ec21bf4e025fea7a311121ac2c271c235eb
ss7424Refar <812647742@qq.com>
2018-09-25 18:02:23 +0800

    add Task modal_20180925

--- application/index/controller/TaskManager.php
+++ application/index/controller/TaskManager.php
@@ -12,7 +12,72 @@ use think\Controller;
 
 
 class TaskManager extends Controller{
-    
+    /*
+     * read machine info
+     */
+    public function readMachineInfo(){
+        $query = $this->request->param('q');
+        $file = fopen(ATS_PREPARE_PATH. ATS_PREPARE_FILE. ATS_FILE_suffix,'r');
 
+        $jsonResult=array();
+        $line=0;
+        while ($data = fgetcsv($file)) { //每次读取CSV里面的一行内容
+            $line++;
+            if ($line>=2){
+                $machineId=$data[2];
+                $appendedTestMachine=$data[1]. "(". $data[2]. ")" ;
+                if (empty(trim($query))) {
+                    $tmpArray = array('id' => $data[1], 'text' => $appendedTestMachine);
+                    array_push($jsonResult, $tmpArray);
+                }else {
+                    if (stristr($appendedTestMachine, $query) !== false){
+                        $tmpArray = array('id' => $data[1], 'text' => $appendedTestMachine);
+                        array_push($jsonResult, $tmpArray);
+                    }
+
+                }
+
+            }
+
+        }
+        echo json_encode($jsonResult);
+
+        fclose($file);
+
+    }
+
+    public function readTestPCDetail(){
+        $file = fopen(ATS_PREPARE_PATH. ATS_PREPARE_FILE. ATS_FILE_suffix,'r');
+
+        $machineId = $this->request->param('machineId');;
+
+        $jsonResult=array();
+        $line=0;
+
+        if(!empty($machineId)){
+            while ($data = fgetcsv($file)) { //每次读取CSV里面的一行内容
+                $line++;
+                if ($line>=2){
+                    if ($data[2] == $machineId){
+//                        $tmpArray = array('product'=> $data[6], 'sn' => $data[7], 'pn' => $data[8], 'oem' => $data[9], 'sys'=> $data[10], 'lanIp' => $data[3], 'shelfId' => $data[0], 'bios'=>$data[11]);
+//                        array_push($jsonResult, $tmpArray);
+                        $jsonResult[] = $data[6];
+                        $jsonResult[] = $data[7];
+                        $jsonResult[] = $data[8];
+                        $jsonResult[] = $data[9];
+                        $jsonResult[] = $data[10];
+                        $jsonResult[] = $data[3];
+                        $jsonResult[] = $data[0];
+                        $jsonResult[] = $data[11];
+                        break;
+                    }
+                }
+
+            }
+
+        }
+        echo json_encode($jsonResult);
+        fclose($file);
+    }
 
 }
