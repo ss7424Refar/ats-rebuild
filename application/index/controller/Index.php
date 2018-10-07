c869198f55d9b7f2969cfc7e939440ca8dfa97d6
ss7424Refar <812647742@qq.om>
2018-10-07 16:38:26 +0800

    add [detail modal]_20181007

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,19 +8,28 @@
 
 namespace app\index\controller;
 
+use think\Request;
+
 /*
  * 控制页面跳转
  */
 class Index extends Common
 {
 
-
     public function TaskManager(){
         return $this->fetch();
 
     }
-    public function ToolAdd($id){
-        $taskId = $this->request->param('selection');
+    public function ToolAdd(){
+        // 防止浏览器恶意输入
+        if (null == Request::instance()->server('HTTP_REFERER')){
+            $this->error('please click link from taskManager', url('Index/TaskManager'));
+            return;
+        }
+        $taskId = $this->request->param('taskId');
+
+        // 模板变量赋值
+        $this->assign('taskId',$taskId);
 
         return $this->fetch();
 
