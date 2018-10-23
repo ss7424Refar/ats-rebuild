5f576a8a8e0e359fd37114726b80eeb5bb7cf797
ss7424Refar <812647742@qq.om>
2018-10-23 22:45:00 +0800

    modify for noDynamic Branch_20181023

---
+++ application/index/controller/ToolHandler.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: refar
+ * Date: 18-10-23
+ * Time: 下午8:27
+ */
+
+namespace app\index\controller;
+
+use app\index\model\AtsTaskToolSteps;
+use app\index\model\AtsTool;
+use ext\ToolMaker;
+
+class ToolHandler extends Common {
+    public function getTool(){
+        $selection = $this->request->param('selection');// 需要继承控制器
+        $toolId = $this->request->param('toolId');
+        $index = $this->request->param('collapseId');
+
+
+        if (JUMP_START == $selection) {
+            return ToolMaker::getJumpStart($index);
+
+        }
+
+    }
+
+
+}
