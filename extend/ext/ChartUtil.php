ab03c9d7be31a5cead10c2de4ee94ccec42f6c74
ss7424Refar <812647742@qq.com>
2018-12-04 18:08:29 +0800

    modify_echart_machine_20181204

--- extend/ext/ChartUtil.php
+++ extend/ext/ChartUtil.php
@@ -23,4 +23,36 @@ class ChartUtil {
         return $hourArray;
     }
 
+    public static function makeMachineOption($timer, $data) {
+        $start = 0; $end = 0;
+        if (HOUR == $timer) {
+            $start = 0;
+            $end = 24;
+        }
+
+        $serialTimeData = array();
+        for ($i = 0; $i < count($data); $i++) {
+            $key = $data[$i]['hour'];
+            if (array_key_exists($key, $serialTimeData)) {
+                $serialTimeData[$key] += $data[$i]['total'];
+            } else {
+                $serialTimeData[$key] = $data[$i]['total'];
+            }
+        }
+
+        // 填充时间，
+        if (null != $serialTimeData) {
+            for ($k = $start; $k < $end; $k++) {
+                // 键名如果不重复则添加0
+                if (!array_key_exists($k, $serialTimeData)) {
+                    $serialTimeData[$k] = 0;
+                }
+            }
+            // 按照升序排序
+            ksort($serialTimeData);
+            return $serialTimeData;
+        }
+        return $serialTimeData;
+    }
+
 }
