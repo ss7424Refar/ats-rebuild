2f1ede2f844f272cfbbc94b66321e6405e4e404c
ss7424Refar <812647742@qq.com>
2018-12-10 17:29:58 +0800

    add WorkMan framework_20181210

---
+++ server.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: refar
+ * Date: 18-12-10
+ * Time: 下午1:59
+ */
+
+define('APP_PATH', __DIR__ . '/application/');
+define('BIND_MODULE','push/worker');
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';
