39d40182b855be17eb695d47b6e7680f97e06d68
ss7424Refar <812647742@qq.om>
2019-03-30 17:16:16 +0800

    [Update] thinkphp5 framework to 5.0.24_20190330 [Add] phpExcel to create machine_user.xlsx_20190330

--- application/config_dev.php
+++ application/config_dev.php
@@ -290,6 +290,9 @@ return [
         'sql_explain'     => false
     ],
 
+    //数据库配置2 (样机管理系统)
+    'db_config2' => 'mysql://root:123456@192.168.0.29:3306/itd#utf8',
+
     // 配置swiftmailer邮件发送服务器
     'smtp_host'         => '127.0.0.1',
     'smtp_port'         => '25',
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -84,11 +84,22 @@ class ComposerStaticInit34a41e2841af1a67f3ddef099fc7b348
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'P' => 
+        array (
+            'PHPExcel' => 
+            array (
+                0 => __DIR__ . '/..' . '/phpoffice/phpexcel/Classes',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit34a41e2841af1a67f3ddef099fc7b348::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit34a41e2841af1a67f3ddef099fc7b348::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit34a41e2841af1a67f3ddef099fc7b348::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
