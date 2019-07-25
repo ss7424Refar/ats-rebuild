47dd91b2c72cf9dc864f4495dd10380026b00f39
ss7424Refar <812647742@qq.com>
2019-07-25 21:22:33 +0800

    [Add] itd search condition function_20190725

--- application/services/controller/MachineSever.php
+++ application/services/controller/MachineSever.php
@@ -34,21 +34,60 @@ class MachineSever extends Controller {
         $pageSize = $this->request->param('limit');
         $offset = $this->request->param('offset');
 
+        $formData = $this->request->param('formData');
+
+        $map = array(); // 查询条件
+
+        if ($formData) {
+            $formData = json_decode($formData);
+            if (!empty($formData->fixed)) {
+                $map['fixed_no'] = $formData->fixed;
+            }
+            if (!empty($formData->names)) {
+                $map['MODEL_NAME'] = ['like', '%' . $formData->names . '%'];
+            }
+            if (!empty($formData->serial)) {
+                $map['SERIAL_NO'] = $formData->serial;
+            }
+            if (!empty($formData->type)) {
+                $map['type'] = $formData->type;
+            }
+            if (!empty($formData->user)) {
+                $map['user_name'] = $formData->user;
+            }
+            if (!empty($formData->location)) {
+                $map['location'] = $formData->location;
+            }
+            if (!empty($formData->status)) {
+                $map['model_status'] = $formData->status;
+            }
+            if (!empty($formData->depart)) {
+                $map['department'] = $formData->depart;
+            }
+            if (!empty($formData->section)) {
+                $map['section_manager'] = $formData->section;
+            }
+        }
+
+
         $jsonRes = array();
 
-        $res = Db::query('select fixed_no, MODEL_NAME, model_status, user_name, department, section_manager, 
-                                start_date, predict_date from itd.d_main_engine order by instore_date desc limit ?, ?', [$offset, $pageSize]);
+//        $res = Db::query('select fixed_no, MODEL_NAME, model_status, user_name, department, section_manager,
+//                                start_date, predict_date from itd.d_main_engine order by instore_date desc limit ?, ?', [$offset, $pageSize]);
 
+        $res = Db::table('itd.d_main_engine')->where($map)->order('instore_date desc')->limit($offset, $pageSize)->select();
         for ($i = 0; $i < count($res); $i++) {
             $res[$i]['model_status'] = $this->statusArray[$res[$i]['model_status']];
             $res[$i]['department'] = $this->departArray[$res[$i]['department']];
             $res[$i]['section_manager'] = $this->sectionArray[$res[$i]['section_manager']];
         }
 
-        $res2 = Db::query('select count(*) as total from itd.d_main_engine');
+//        $res2 = Db::query('select count(*) as total from itd.d_main_engine');
+        $total = Db::table('itd.d_main_engine')->where($map)->count();
 
-        $jsonRes['total'] = $res2[0]['total'];
+        $jsonRes['total'] = $total;
         $jsonRes['rows'] = $res;
         return json($jsonRes);
     }
+
 }
